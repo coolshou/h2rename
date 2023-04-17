f8a02e53facfe925db23cd408210cd35ec222b4d
coolshou <coolshou2060@gmail.com>
2023-04-17 11:52:22 +0800

    fix translate

---
+++ lang/h2rename_zh_TW.ts
@@ -0,0 +1,316 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>H2rename</name>
+    <message>
+        <location filename="../src/h2rename.cpp" line="56"/>
+        <source>Regel &amp;einfügen</source>
+        <translation>(&amp;E)插入規則</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="57"/>
+        <source>Ctrl+E</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="63"/>
+        <source>Regel(n) &amp;löschen</source>
+        <translation>删除規則(&amp;L)</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="64"/>
+        <source>Ctrl+L</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="70"/>
+        <source>&amp;Regel erzeugen</source>
+        <translation>創建規則(&amp;R)</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="71"/>
+        <source>Ctrl+R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="142"/>
+        <source>%1 %2, %3 %4</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="142"/>
+        <source>Name</source>
+        <translation>名稱</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="142"/>
+        <source>Namen</source>
+        <translation>名稱</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="142"/>
+        <source>Änderung</source>
+        <translation>改變</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="142"/>
+        <source>Änderungen</source>
+        <translation>變化</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="145"/>
+        <source>%1 %2!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="145"/>
+        <source>Namenskollision</source>
+        <translation>名稱衝突</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="145"/>
+        <source>Namenskollisionen</source>
+        <translation>名稱衝突</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="152"/>
+        <source>Verzeichnis wählen</source>
+        <translation>選擇目錄</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="174"/>
+        <location filename="../src/h2rename.cpp" line="260"/>
+        <source>H2rename</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="235"/>
+        <source>Sie sind im Begriff, %1 %2 umzubenennen.</source>
+        <translation>即將重新命名 %1 %2.</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="235"/>
+        <source>Datei/Verzeichnis</source>
+        <translation>文件/目錄</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="235"/>
+        <source>Dateien/Verzeichnisse</source>
+        <translation>文件/目錄</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="237"/>
+        <source> %1 %2 aufgrund von Namenskollisionen nicht umbenannt.</source>
+        <translation> 由於名稱衝突，%1 %2 未重命名.</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="239"/>
+        <source>wird</source>
+        <translation>成為</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="239"/>
+        <source>werden</source>
+        <translation>變得</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="240"/>
+        <source> Fortfahren?</source>
+        <translation> 繼續?</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="242"/>
+        <source>H2rename - Umbenennen starten?</source>
+        <translation>H2rename - 開始重新命名？</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="246"/>
+        <source>Umbenennen läuft ...</source>
+        <translation>正在重新命名...</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.cpp" line="261"/>
+        <source>Konnte %1 &quot;%2&quot; nicht in &quot;%3&quot; umbenennen.</source>
+        <translation>無法將 %1 \&quot;%2\&quot; 重命名為 \&quot;%3\&quot;.</translation>
+    </message>
+</context>
+<context>
+    <name>H2renameClass</name>
+    <message>
+        <location filename="../src/h2rename.ui" line="14"/>
+        <source>H2rename 0.8.2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="54"/>
+        <source>Verzeichnis</source>
+        <translation>目錄</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="64"/>
+        <source>Durchsuchen ...</source>
+        <translation>搜索...</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="74"/>
+        <source>mit Unterverzeichnissen</source>
+        <translation>包含子目錄</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="84"/>
+        <source>Laden</source>
+        <translation>載入</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="105"/>
+        <source>Regel einfügen</source>
+        <translation>插入規則</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="115"/>
+        <source>Regel(n) löschen</source>
+        <translation>刪除規則</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="152"/>
+        <source>Zeige</source>
+        <translation>展示</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="160"/>
+        <source>alle</source>
+        <translation>所有</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="165"/>
+        <source>alle Änderungen</source>
+        <translation>所有的變化</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="170"/>
+        <source>Wirkung selektierter Regeln</source>
+        <translation>所選規則的效果</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="175"/>
+        <source>Namenskollisionen</source>
+        <translation>名稱衝突</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="226"/>
+        <source>Umbenennen</source>
+        <translation>改名</translation>
+    </message>
+    <message>
+        <location filename="../src/h2rename.ui" line="304"/>
+        <source>&amp;copy; 2009 by Harald Bögeholz / c&apos;t &lt;a href=&quot;http://www.ctmagazin.de/&quot;&gt;www.ctmagazin.de&lt;/a&gt;</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Name</name>
+    <message>
+        <location filename="../src/name.h" line="32"/>
+        <source>Verzeichnis</source>
+        <translation>目錄</translation>
+    </message>
+    <message>
+        <location filename="../src/name.h" line="34"/>
+        <source>Anwendung</source>
+        <translation>應用</translation>
+    </message>
+    <message>
+        <location filename="../src/name.h" line="36"/>
+        <source>Datei</source>
+        <translation>文件</translation>
+    </message>
+</context>
+<context>
+    <name>NamesModel</name>
+    <message>
+        <location filename="../src/namesmodel.cpp" line="123"/>
+        <source>FIXME</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/namesmodel.cpp" line="143"/>
+        <source>Originalname</source>
+        <translation>原名稱</translation>
+    </message>
+    <message>
+        <location filename="../src/namesmodel.cpp" line="143"/>
+        <source>Neuer Name</source>
+        <translation>新名稱</translation>
+    </message>
+</context>
+<context>
+    <name>ReadDirProgressDialog</name>
+    <message>
+        <location filename="../src/ReadDirProgress.ui" line="13"/>
+        <source>H2rename - lese Verzeichnisse</source>
+        <translation>H2rename - 讀取目錄</translation>
+    </message>
+    <message>
+        <location filename="../src/ReadDirProgress.ui" line="19"/>
+        <source>Lese gerade</source>
+        <translation>現在讀取</translation>
+    </message>
+    <message>
+        <location filename="../src/ReadDirProgress.ui" line="35"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ReadDirProgress.ui" line="75"/>
+        <source>Abbrechen</source>
+        <translation>打斷</translation>
+    </message>
+</context>
+<context>
+    <name>ReadDirThread</name>
+    <message>
+        <location filename="../src/readdirthread.cpp" line="38"/>
+        <source>Das Verzeichnis &apos;%1&apos; existiert nicht</source>
+        <translation>目錄 &apos;%1&apos; 不存在</translation>
+    </message>
+</context>
+<context>
+    <name>RenameRulesModel</name>
+    <message>
+        <location filename="../src/renamerulesmodel.cpp" line="94"/>
+        <source>ACHTUNG: Suchstring leer!</source>
+        <translation>注意：搜索字符串為空！</translation>
+    </message>
+    <message>
+        <location filename="../src/renamerulesmodel.cpp" line="96"/>
+        <source>ACHTUNG: Ersetzungsstring enthält einen Backslash (\)</source>
+        <translation>注意：替換字符串包含反斜杠 (\)</translation>
+    </message>
+    <message>
+        <location filename="../src/renamerulesmodel.cpp" line="98"/>
+        <source>ACHTUNG: Ersetzungsstring enthält einen Schrägstrich (/)</source>
+        <translation>注意：替換字符串包含斜杠 (/)</translation>
+    </message>
+    <message>
+        <location filename="../src/renamerulesmodel.cpp" line="100"/>
+        <source>ACHTUNG: Ersetzungsstring enthält einen Doppelpunkt!</source>
+        <translation>注意：替換字符串包含冒號！</translation>
+    </message>
+    <message>
+        <location filename="../src/renamerulesmodel.cpp" line="135"/>
+        <source>Ersetze</source>
+        <translation>代替</translation>
+    </message>
+    <message>
+        <location filename="../src/renamerulesmodel.cpp" line="135"/>
+        <source>durch</source>
+        <translation>成為</translation>
+    </message>
+    <message>
+        <location filename="../src/renamerulesmodel.cpp" line="135"/>
+        <source>Regel automatisch erzeugt aus</source>
+        <translation>規則自動生成</translation>
+    </message>
+</context>
+</TS>
